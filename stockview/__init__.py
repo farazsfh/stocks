23b671f585e6efe502cdf3cd700ef40e4aa79cb5
Faraz <15679360+farazsfh@users.noreply.github.com>
2020-08-11 22:43:53 -0400

    Remove celery and secret key

--- stockview/__init__.py
+++ stockview/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'abc'
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 from stockview import routes
